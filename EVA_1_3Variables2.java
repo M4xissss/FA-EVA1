ea5280bb0fbf5875df13ef7966cca46a877f06e6
DamiannnMxis <L24550379@chihuahua2.tecnm.mx>
2024-08-28 16:10:00 -0700

    Add files via upload

---
+++ EVA_1_3Variables2.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package EVA_1_3Variables2;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA_1_3Variables2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.println("HOLA MUNDO!!");//Cadena de texto
+    System.out.println(100);//Entero
+    System.out.println(3.1416);//Número con punto flotante
+    System.out.println('c');//Caracter
+    System.out.println(true);//Valores lógicos volátiles (booleanos)
+    System.out.println(
+            
+            false)
+            
+            ;//;--> FIN DE INSTRUCCIÓN
+    // ENTEROS:
+    // int 
+    // long
+    // short
+    
+    // NUMEROS CON PARTE DECIMAL:
+    // float
+    // double
+    
+    // CADENAS DE TEXTO:
+    // String
+    
+    // CARACTERES:
+    // char
+    
+    // VALORES LOGICOS:
+    // Boolean
+    
+    // DECLARACIÓN DE VARIABLES:
+    // Sintaxis para declarar variables (REGLAS)
+    // Tipo de dato nombre de la variable 
+    
+    String nombre;
+    String apellido;
+    double promedio;
+int edad;
+
+    
+    edad = 18; //ASIGNACIÓN: estoy colocando el valor 18
+    //Dentro de la variable edad
+    
+    nombre = "DAMIÁN EDUARDO";
+    apellido = "ISAÍAS DE LUNA";
+    promedio = 8.8;
+        
+    System.out.println(edad);
+    System.out.println(nombre);
+    System.out.println(apellido);
+    System.out.println(promedio);
+    
+    //VEHÍCULOS
+    String marca;
+    String modelo; 
+    int year; //la ñ en otros lenguajes no es aceptada
+    double precio;
+    
+    //ASIGNACIÓN
+    marca = "NISSAN"; 
+    modelo = "GTR";
+    year = 2008;
+    precio = 1000000.1; 
+    
+    System.out.println(marca);
+    System.out.println(modelo);
+    System.out.println(year);
+    System.out.println(precio);
+
+    }
+        }
+
