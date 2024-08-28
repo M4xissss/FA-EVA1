45be305e64f6cce6c6c1a1a10b9cc2fcaff495aa
DamiannnMxis <L24550379@chihuahua2.tecnm.mx>
2024-08-28 16:34:04 -0700

    Add files via upload

---
+++ EVA1_4TV.java
@@ -0,0 +1,45 @@
+/*
+
+ */
+package eva1_4tv;
+
+/**
+ *
+ * @author eddyi
+ */
+public class EVA1_4TV {
+
+    /**
+     * @param args the command line arguments
+     */
+
+        
+public static void main(String[] args) {
+
+//DECLARACIÓN DE VARIABLES
+    String marca;
+    String modelo; 
+    String tipo;
+    int tamaño; 
+    double precio;
+            
+     //ASIGNARLES UN VALOR
+     marca = "Sony";
+     modelo = "Briavia";
+     tipo = "QLED";
+     tamaño = 100;
+     precio = 50000.9;
+     
+     //IMPRIMIR EL VALOR 
+     System.out.println(marca);
+     System.out.println(modelo);
+     System.out.println(tipo);
+     System.out.println(tamaño);
+     System.out.println(precio);        
+     
+    
+
+    
+    }
+    
+}
