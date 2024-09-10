0d208444a67d19ee65f4c53936fcc63b869d0def
Damian Isaías <L24550379@chihuahua2.tecnm.mx>
2024-09-10 16:33:58 -0700

    Add files via upload

---
+++ EVA1_15_AGUILA_SELLO.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_15_aguila_sello;
+
+/**
+ *
+ * @author eddyi
+ */
+public class EVA1_15_AGUILA_SELLO {
+
+    public static void main(String[] args) {
+   
+        System.out.println(Math.random());
+        
+double valor = Math.random();
+if(valor < 0.5){
+System.out.println("Aguila");
+}else{
+System.out.println("Sello");
+}
+    }
+}
