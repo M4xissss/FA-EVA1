b6afd10b6c1804a9fe4be897acc4cb32ad0bd08e
Damian Isaías <L24550379@chihuahua2.tecnm.mx>
2024-09-26 16:33:24 -0700

    Add files via upload

---
+++ EVA1_22_SWITCH.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_22_switch;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author eddyi
+ */
+public class EVA1_22_SWITCH {
+
+    public static void main(String[] args) {
+         int days;
+         Scanner captu = new Scanner(System.in);
+         System.out.println("Captura el número del día de la semana:");
+         days = captu.nextInt();
+         captu.nextLine();
+         
+        //ELEGIR ENTRE MÚLTIPLES ALTERNATIVAS
+         switch(days){
+             case 1: 
+                 System.out.println("Domingo");
+                 break; //interrumpir el switch (lo termina)
+                      case 2: 
+                 System.out.println("Lunes");
+                 break; //interrumpir el switch (lo termina)
+                      case 3: 
+                 System.out.println("Martes");
+                 break; //interrumpir el switch (lo termina)
+                      case 4: 
+                 System.out.println("Miércoles");
+                 break; //interrumpir el switch (lo termina)
+                      case 5: 
+                 System.out.println("Jueves");
+                 break; //interrumpir el switch (lo termina)
+                      case 6: 
+                 System.out.println("Viernes");
+                 break; //interrumpir el switch (lo termina)
+                      case 7: 
+                 System.out.println("Sábado");
+                 break; //interrumpir el switch (lo termina)
+                      default: //Opcional, se ejecuta cuando no existe una opción
+                       //no tiene break, Y ES LA ÚLTIMA INSTRUCCIÓN DEL SWITCH
+                          System.out.println("Número no válido (solo del 1 al 7)");
+                          }
+    }
+}
